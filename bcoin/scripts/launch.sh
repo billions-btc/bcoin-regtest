90408c9e235f2f1e09dbcbc91696726a5586b5a3
Aman Saran <aman06@gmail.com>
2021-05-11 02:51:19 -0400

    remove coinbase address

--- bcoin/scripts/launch.sh
+++ bcoin/scripts/launch.sh
@@ -5,4 +5,5 @@ echo 'Launching mine'
 /code/scripts/mine.sh &
 
 # Start the bcoin node
-node /code/bcoin/bin/node --network=regtest --http-host=0.0.0.0 --wallet-http-host=0.0.0.0 --api-key=secretpw --max-outbound=1 --coinbase-address=RLzyQ3AAHEdijrZvmTPWsWrSi8YwZ8VV3T
+node /code/bcoin/bin/node --network=regtest --http-host=0.0.0.0 --wallet-http-host=0.0.0.0 --api-key=secretpw --max-outbound=1 
+
